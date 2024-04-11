940f5c825fa27d0d2267846d2a8ddc7987b6e458
Rafael Guerra <rafa99gm@hotmail.com>
2024-04-11 10:23:54 +0100

    solved

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Ready to go! 🚀");
+
+let i = 0;
+
+while (i < 30) {
+  if (i === 10) {
+    console.log('ten')
+  }
+  if (i === 20) {
+    console.log('twenty');
+  }
+  i++;
+  console.log(i);
+}
+
